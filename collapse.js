463eb45c3a55d924a71ff5d2176a32f9f3148135
kshu <shu.kevins@gmail.com>
2021-02-13 17:46:41 -0500

    fixed blog url

--- collapse.js
+++ collapse.js
@@ -11,10 +11,13 @@ function toggleCollapse() {
     }
 }
 
+// The issue is that if the thing is collapsed, and then you resize until it is in full screen mode size, then there'll just be no header.
 function redisplay() {
     if (!window.matchMedia("(max-width: 800px").matches) {
-        document.getElementById("homeSidebar").style.display = "block";
+            document.getElementById("homeSidebar").style.display = "block";
     } else {
-        document.getElementById("homeSidebar").style.display = "none";
+        if (collapsed) {
+            document.getElementById("homeSidebar").style.display = "none";
+        }
     }
 }
